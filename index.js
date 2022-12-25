6bcd22cfc23ca3d5c094044008626ada07d1ae32
Adeola Ephraim <dunsin.bada@gmail.com>
2022-12-25 08:54:05 +0100

    [ADD] addedapi rate limit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 require('dotenv').config();
 
 const PORT = (process.env.PORT) ? process.env.PORT : 3000;
+const RATE_LIMITER = require('./config/rate-limiter');
 
+app.use('/api', RATE_LIMITER);
 let routes = require('./routes/api/v1');
 app.use(routes);
 
