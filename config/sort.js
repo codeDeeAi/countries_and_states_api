df326a2a50b356368c8300d31240582ff5492ea1
Adeola Ephraim <dunsin.bada@gmail.com>
2022-12-25 16:21:31 +0100

    [ADD] added sort by direction

---
+++ config/sort.js
@@ -0,0 +1,41 @@
+/**
+ * Compare Values 
+ * @param {String} key 
+ * @param {String} order asc or desc
+ * @returns 
+ */
+const compareValues = (key, order = 'asc') => {
+    return function innerSort(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            return 0;
+        }
+
+        const varA = (typeof a[key] === 'string') ?
+            a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string') ?
+            b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return (
+            (order === 'desc') ? (comparison * -1) : comparison
+        );
+    };
+};
+/**
+ * Sort the array by direction
+ * @param {string} sortParam 
+ * @param {string} order asc or desc
+ * @param {array} array 
+ * @return {array} array 
+ */
+const sortByDirection = (sortParam, order = 'asc', array) => {
+    return array.sort(compareValues(sortParam, order));
+};
+
+module.exports = sortByDirection;
